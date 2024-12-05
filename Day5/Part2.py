072d1d7abf3b0e8b71397f3416878462940383aa
alex <al@pop-os.lan>
2024-12-05 12:15:30 -0500

    Cleaned up my test stuffs for part 2

--- Day5/Part2.py
+++ Day5/Part2.py
@@ -17,17 +17,6 @@ def eval(page, preceders, update):
             return False
     return True
 
-'''
-def check(update):
-    for i, page in enumerate(update):
-        for post in update[i:]:
-            if page not in precedents:
-                continue
-            if post in precedents[page]:
-                return False
-    return True
-'''
-
 def check(update):
     for page in update:
         if page not in precedents:
@@ -38,38 +27,26 @@ def check(update):
 
 def correct(update):
     for index, page in enumerate(update):
-        #if index == 0:
-        #    continue
         if page not in precedents:
             continue
         for pre in precedents[page]:
             if pre not in update:
                 continue
-            #print(update.index(pre), pre, index, page)
             if update.index(pre) > index:
                 while not eval(page, precedents[page], update):
-                    #print(f"need to update {index}:{page}")
                     pagei = update.index(page)
                     update[pagei], update[pagei+1] = update[pagei+1], update[pagei]
-                    #print(update)
                 return update
-
-for page in precedents:
-    print(f"{page} preceeded by {precedents[page]}")
 
 running_sum = 0
 for update in updates:
-    if not check(update):
-        #running_sum += int(update[int(len(update)/2)])
-        print(update)
-        while not check(update):
-            #print()
-            #print(update)
-            update = correct(update)
-            #print(update)
-        print(update)
-        print()
-        running_sum += int(update[int(len(update)/2)])
+    if check(update):
+        continue
 
+    while True:
+        update = correct(update)
+        if check(update):
+            running_sum += int(update[int(len(update)/2)])
+            break
 
 print(running_sum)
