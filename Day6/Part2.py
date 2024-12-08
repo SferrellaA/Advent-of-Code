db0739956bbbe76c5df0d620e7c76cc93f65bb6e
SferrellaA <alexander@sferrella.com>
2024-12-07 21:18:54 -0500

    Cleaned up part 2
    I like programming recursively, but when I initially tried to solve part 2 this way I was missing a few concepts. I wasn't thinking that about how the way you face the block doesn't matter; if you've previously checked if a blocker doesn't work at a coordinate, it doesn't matter if you reach that point in the future; you've already proven it doesn't work. If you block it _later_, you'll have been blocking yourself earlier on. I didn't think of this at first, and so re-wrote my part 1 to not be recursive (maybe should have done this anyway, since I'd had to increase the recursion depth). Took a while to run part 2, but I don't think it's _quite_ brute force. Not the most efficient algorithm though.
    
    I'd had to take a few breaks solving this part because the wife was getting upset I was coding for so long. May not be able to spend much time coding at home anymore... Life doesn't pause, even if you're _really close_ to solving the problem.

--- Day6/Part2.py
+++ Day6/Part2.py
@@ -1,7 +1,3 @@
-# Sample has 6 spots
-from sys import setrecursionlimit
-setrecursionlimit(10**7) # don't judge me
-
 from grid import read_stdin
 grid, guard = read_stdin("^")
 grid[guard] = "."
@@ -26,18 +22,6 @@ def rotate(face):
     if face == "^":
         return ">"
 
-'''
-def step(row, col, face):
-    facing = ahead(row, col, face)
-    if facing not in grid:
-        return True, None
-    if grid[facing] == "#":
-        face = rotate(face)
-    else:
-        row, col = facing
-    return False, (row, col, face)
-'''
-
 def scan(row, col, face):
     potential = ahead(row, col, face)
     path = []
@@ -53,7 +37,6 @@ def scan(row, col, face):
         else:
             row, col = facing
 
-
 row, col = guard
 face = "^"
 blockers = []
@@ -61,11 +44,10 @@ failures = []
 
 while True:
     facing = ahead(row, col, face)
-    print((row, col, face))
+    #print((row, col, face))
     if facing not in grid:
         break
     if grid[facing] == "#":
-        #print("#")
         face = rotate(face)
         continue
     if facing not in blockers and facing not in failures:
@@ -75,93 +57,5 @@ while True:
             failures.append(facing)
     row, col = facing
 
-print(blockers)
 print(len(blockers))       
-# 1835 is too high, 1746 too low; 1784 was it
-
 # man this one was bullshit
-
-
-
-
-'''
-def scan(row, col, face, path, start):
-    if (row, col) not in grid:
-        #print(path)
-        return False
-    if len(path) > 0 and (row, col) == start:
-        return True
-    if grid[(row, col)] == "#":
-        return "#"
-    if (row, col, face) in path:
-        return True
-    path.append((row, col, face))
-    report = None
-    if face == "^":
-        report = scan(row-1, col, face, path, start)
-        if report == "#":
-            report = scan(row, col+1, ">", path, start)
-    if face == ">":
-        report = scan(row, col+1, face, path, start)
-        if report == "#":
-            report = scan(row+1, col, "v", path, start)
-    if face == "v":
-        report = scan(row+1, col, face, path, start)
-        if report == "#":
-            report = scan(row, col-1, "<", path, start)
-    if face == "<":
-        report = scan(row, col-1, face, path, start)
-        if report == "#":
-            report = scan(row-1, col, "^", path, start)
-    if report == "#":
-        # what?
-        return False
-    return report
-
-potentials = []
-
-def step(row, col, face):
-    if (row, col) not in grid:
-        return "done"
-    rotate = False
-    report = None
-    if grid[(row, col)] == "#":
-        rotate = True
-    if face == "^":
-        if rotate:
-            return (row+1, col+1, ">")
-        if scan(row, col, ">", [], (row, col)):
-            potentials.append((row, col, face))
-        return (row-1, col, face)
-    if face == ">":
-        if rotate:
-            return (row+1, col-1, "v")
-        if scan(row, col, "v", [], (row, col)):
-            potentials.append((row, col, face))
-        return (row, col+1, face)
-    if face == "v":
-        if rotate:
-            return (row-1, col-1, "<")
-        if scan(row, col, "<", [], (row, col)):
-            potentials.append((row, col, face))
-        return (row+1, col, face)
-    if face == "<":
-        if rotate:
-            return (row-1, col+1, "^")
-        if scan(row, col, "^", [], (row, col)):
-            potentials.append((row, col, face))
-        return (row, col-1, face)
-
-row, col = guard
-face = "^"
-while True:
-    #print(row, col, face)
-    report = step(row, col, face)
-    if report == "done":
-        break
-    row, col, face = report
-
-print(potentials)
-print(len(potentials))
-'''
-
