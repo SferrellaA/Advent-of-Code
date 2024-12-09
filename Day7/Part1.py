2b889664f925dfab23cb7202b8f3c744b7143b87
alex <al@pop-os.lan>
2024-12-08 22:01:45 -0500

    Cleaning up part 1

--- Day7/Part1.py
+++ Day7/Part1.py
@@ -1,9 +1,3 @@
-'''
-190: 10 19 has only one position that accepts an operator: between 10 and 19. Choosing + would give 29, but choosing * would give the test value (10 * 19 = 190).
-3267: 81 40 27 has two positions for operators. Of the four possible configurations of the operators, two cause the right side to match the test value: 81 + 40 * 27 and 81 * 40 + 27 both equal 3267 (when evaluated left-to-right)!
-292: 11 6 16 20 can be solved in exactly one way: 11 + 6 * 16 + 20
-'''
-
 from sys import stdin
 calibrations = []
 for line in stdin:
@@ -13,15 +7,9 @@ for line in stdin:
 calibration = 0
 
 def evaluate(value, target, operators, operands):
-    global calibration
-    # There goes my attempt to spare some cycles...
-    # Too tired to tell whaat this is doing that casues it to skip things that work
-    #if value + sum(operators) > target:
-    #    return False
-
     if operators == []:
         if value == target:
-            print(f"\t{operands}")
+            global calibration
             calibration += target
             return True
         return False
@@ -32,10 +20,6 @@ def evaluate(value, target, operators, operands):
 
 for equation in calibrations:
     target, operators = equation
-    print(equation)
     evaluate(operators[0], target, operators[1:], [])
 
 print(calibration)
-# 20665820890776 also too low
-# 20665820948644 is too low
-# 20665830408335
